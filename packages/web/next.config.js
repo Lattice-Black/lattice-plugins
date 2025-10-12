fb06b2bfd9e03b07d769a4b50a339b46b0f46b62
Cary Wolff <boss@caryyon.com>
2025-10-12 13:59:39 -0500

    Remove webpack externals to fix instrumentation ESM/CommonJS compatibility

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -7,19 +7,6 @@ const nextConfig = {
   experimental: {
     instrumentationHook: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer && config.name !== 'edge-server') {
-      // Mark server-only packages as externals for Node.js runtime only
-      // Don't apply to Edge Runtime (middleware) which can't use Node.js modules
-      config.externals = config.externals || [];
-      config.externals.push({
-        '@lattice.black/plugin-nextjs': 'commonjs @lattice.black/plugin-nextjs',
-        '@lattice.black/core': 'commonjs @lattice.black/core',
-        'glob': 'commonjs glob',
-      });
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
